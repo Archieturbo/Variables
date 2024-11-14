163fdf4d061c028fc7d2b64f433674ad728d3d69
Archieturbo <archieturbo@live.se>
2024-11-14 15:39:15 +0100

    ny commit

---
+++ variabler/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace variabler;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, allihopa!");
+    }
+}
